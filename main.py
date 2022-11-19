690a5dc26a3a0392887aa86466ae6879baa6b4d2
Leo Zhi <104808326+LeoZhii@users.noreply.github.com>
2022-11-19 15:32:21 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
-# Submission 1: Image Processing Module
-# Nov 14
-# Jake Choi and Leo Zhi
+import pygame
 
+###############################################################
+# Keep this block at the beginning of your code. Do not modify.
+def initEnv():
+    print("\nWelcome! Before we start...")
+    env = input("Are you using mu w/pygame0 (m), replit (r) or idle (i)? ").lower()
+    while env not in "mri":
+        print("Environment not recognized, type again.")
+        env = input("Are you using mu w/pygame0 (m), replit (r) or idle (i)? ").lower()
+    print("Great! Have fun!\n")
+    return env
+
+# Use the playSound() function below to play sounds. 
+# soundfilename does not include the .wav extension, 
+# e.g. playSound(apples,ENV) plays apples.wav
+def playSound(soundfilename,env):
+    if env == "m":
+        exec("sounds." + soundfilename + ".play()")
+    elif env == "r":
+        from replit import audio
+        audio.play_file("sounds/"+soundfilename+".wav")
+    elif env == "i":
+        pygame.mixer.init()
+        pygame.mixer.music.load("sounds/"+soundfilename+".wav")
+        pygame.mixer.music.play()
+
+ENV = initEnv()
+###############################################################
